29251084e496fe72ce60060f1782d64de50480fb
MichaelClaytonNoble <makos.code@gmail.com>
2021-06-13 19:46:00 -0700

    tournament winner

--- Arrays/tournament_winner.js
+++ Arrays/tournament_winner.js
@@ -1,27 +1,24 @@
 function tournamentWinner(competitions, results) {
-  // Write your code here.
-	
-	//competitions[i] = [hometeam, awayteam]
-	//results[i] = ([1] === hometeam wins ) || ([0] === awayteam wins)
-	
+	//competitions[i] = [homeTeam, awayTeam]
+	  //results[i] = ([1] === homeTeam wins ) || ([0] === awayteam wins)
+
 	//catalog results of tourney by 'team name'
 	//calculate how many wins each team has
 	let catalog = {};
 	let currentWinner = '';
 	for(let i = 0; i < competitions.length; i++){
+
 		let homeTeam = competitions[i][0];
 		let awayTeam = competitions[i][1];
 		let winner = '';
 		winner = results[i] === 1 ? homeTeam : awayTeam;
 		competitions[winner] = competitions[winner] + 3 || 3;
 
-		currentWinner = (competitions[winner] > competitions[currentWinner] ? winner : currentWinner) || winner
+		currentWinner = (competitions[winner] > competitions[currentWinner] ? winner : currentWinner) || winner;
 	}
-	
-	//return the team with the most wins. 
+
+	//return the team with the most wins
 	return currentWinner;
-	
-  return '';
 }
 
 // Do not edit the line below.
